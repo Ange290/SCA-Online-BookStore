1eeb6563f90d2c071c8ad42b55eed212abfab458
Ange290 <uange209@gmail.com>
2024-03-28 09:42:05 +0200

    Update: User Queries

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,18 +1,18 @@
-const { model } = require('mongoose');
+const mongoose = require('mongoose');
 const userModel = require('../model/user_model.js');
 const bcrypt = require('bcrypt');
 const UserController ={
     UserRegistration : async(req,res)=>{
         try {
             const hashedPassword = await bcrypt.hash(req.body.password, 10);
-            const user= new Users({
+            const user= new userModel({
                 name:req.body.name,
                 email: req.body.email,
                 password: hashedPassword,
                 num:req.body.num,
             });
             const savedUser = await user.save();
-            res.status(201).json(savedUser);
+            res.status(201).json({message:'User Created Successfully'});
         } catch (error) {
            res.status(500).json(error) 
         }
