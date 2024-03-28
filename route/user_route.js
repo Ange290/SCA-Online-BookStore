5c92f889852e764e217baee46d91891c9abfe5ad
Ange290 <uange209@gmail.com>
2024-03-28 10:50:36 +0200

    Update Feature

--- route/user_route.js
+++
@@ -1,7 +0,0 @@
-const express= require('express');
-const routes = express.Router();
-const UserController = require('../controller/user_controller.js');
-
-routes.post("/user/create", UserController.UserRegistration);
-routes.get("/user/get", UserController.getUser)
-module.exports = routes;
